dd297edb5ea8bd13be129ac4ee075b5a9dbb7b0c
Aashay-Thakur <thakur.aashay@gmail.com>
2023-10-05 14:53:22 +0530

    added dummy password

--- sql.js
+++ sql.js
@@ -3,7 +3,8 @@ const mysql = require("mysql");
 var con = mysql.createConnection({
 	host: "localhost",
 	user: "root",
-	password: "",
+	password: "test1234",
+	port: "3306",
 });
 
 con.connect(function (err) {
